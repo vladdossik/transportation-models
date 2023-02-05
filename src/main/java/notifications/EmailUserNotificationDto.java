67d7b83d3fed2bb31419be5e82daf34686fbefde
Ilya Stepanov <i.stepanov@spbexchange.ru>
2023-02-05 17:47:51 +0300

    fix: naming of classes

--- src/main/java/notifications/EmailUserNotificationDto.java
+++ src/main/java/notifications/EmailUserNotificationDto.java
@@ -2,9 +2,7 @@ package notifications;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import java.io.File;
-
-public class EmailIdNotification {
+public class EmailUserNotificationDto {
     @Schema(description = "ID пользователя")
     private int uuid;
     @Schema(description = "Сообщение")
