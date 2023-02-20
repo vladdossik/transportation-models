0b14875d09a672751814a2dc6374726a77c2c871
Ilya Stepanov <i.stepanov@spbexchange.ru>
2023-02-21 00:54:34 +0300

    feat: one dto class for email notification

--- src/main/java/notifications/EmailNotificationDto.java
+++ src/main/java/notifications/EmailNotificationDto.java
@@ -1,16 +1,22 @@
 package notifications;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.USE_DEFAULTS)
 public class EmailNotificationDto {
+    @Schema(description = "UUID пользователя")
+    private Integer uuid;
     @Schema(description = "Кому")
     private String to;
     @Schema(description = "Копия")
     private String cc;
     @Schema(description = "Скрытая копия")
     private String bcc;
+    @Schema(description = "Тема письма")
+    private String subject;
     @Schema(description = "Сообщение")
     private String message;
 //    @Schema(description = "Приложенный файл")
