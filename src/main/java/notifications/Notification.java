08894b2b41cb003ec0db37343f24870b6d6012aa
Ilya Stepanov <i.stepanov@spbexchange.ru>
2023-02-21 01:46:28 +0300

    fix: accessLevel to public

--- src/main/java/notifications/Notification.java
+++ src/main/java/notifications/Notification.java
@@ -1,14 +1,14 @@
 package notifications;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
-@Data
 @AllArgsConstructor
 public class Notification {
+    @Getter(AccessLevel.PUBLIC)
+    @Setter(AccessLevel.PUBLIC)
     @Schema(description = "UUID пользователя")
     private Integer uuid;
     @NotNull
