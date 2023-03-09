1346495f985ac7049a3bd5463a242e4672563311
Ilya Stepanov <i.stepanov@spbexchange.ru>
2023-03-09 22:53:55 +0300

    fix: deleted unnecessary fields

--- src/main/java/notifications/NotificationDto.java
+++ src/main/java/notifications/NotificationDto.java
@@ -3,7 +3,6 @@ package notifications;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -16,21 +15,6 @@ public class NotificationDto {
 
     @Schema(description = "Тип уведомления")
     private NotificationType type;
-
-    @Schema(description = "Статус уведомления")
-    private NotificationStatus status;
-
-    @Schema(description = "Максимальное количество попыток")
-    private Integer maxAttempts;
-
-    @Schema(description = "Общее количество попыток")
-    private Integer totalAttempts;
-
-    @Schema(description = "Время создания")
-    private LocalDateTime createDate;
-
-    @Schema(description = "Время последнего обновления")
-    private LocalDateTime updateDate;
 
 //    @Schema(description = "Приложенный файл")
 //    private File attachment;
